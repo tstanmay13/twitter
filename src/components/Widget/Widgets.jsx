21012eb36154b969c17da21d121ff9f3fbb2b6ae
tstanmay13 <tstanmay13@gmail.com>
2022-07-16 18:57:17 -0500

    done with feed starting widget

--- src/components/Widget/Widgets.jsx
+++ src/components/Widget/Widgets.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./Widgets.css"
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import SearchIcon from "@mui/icons-material/Search";
 
 const Widgets = () => {
     return (
         <div className="widget">
-            this widget
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon"/>
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"933354946111705097"}/>
+            </div>
         </div>
     );
 }
